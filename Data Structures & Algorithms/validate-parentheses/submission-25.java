0304082d1205eb7931a074feebfe746003401c71
jamiljubran <jamiljubran@gmail.com>
2026-06-03 17:17:44 -0700

    Add: validate-parentheses - submission-25

---
+++ Data Structures & Algorithms/validate-parentheses/submission-25.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+    
+
+        Stack<Character> shtack = new Stack<>();
+        for (char c: s.toCharArray())
+        {
+            if(c == '(' || c == '[' || c == '{')
+            {
+                shtack.push(c);
+            }
+            else
+            {
+                if(shtack.empty())
+                {
+                    return false;
+                }
+
+                char top = shtack.pop();
+
+                if( top == '(' && c != ')')
+                {
+                    return false;
+                }
+
+                if (top == '[' && c!= ']')
+                {
+                    return false;
+                }
+
+                if (top == '{' && c!= '}')
+                {
+                    return false;
+                }
+
+               
+
+            }   
+
+
+        }
+         return shtack.empty();
+    }
+}
+

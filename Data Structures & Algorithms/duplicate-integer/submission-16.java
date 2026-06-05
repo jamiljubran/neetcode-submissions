c766b425e65ed0727daa6423be1a448cc0945fa9
jamiljubran <jamiljubran@gmail.com>
2026-06-05 15:44:08 -0700

    Add: duplicate-integer - submission-16

---
+++ Data Structures & Algorithms/duplicate-integer/submission-16.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        for(int i = 0; i < nums.length; i++)
+        {
+            for(int k = 0; k < i; k++)
+            {
+                if( nums[i] == nums[k])
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}

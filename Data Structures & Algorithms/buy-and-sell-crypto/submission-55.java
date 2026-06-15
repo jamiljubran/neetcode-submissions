a4d390de181f08e7ac22eb608c67e31cf3e5e6f9
jamiljubran <jamiljubran@gmail.com>
2026-06-15 14:26:17 -0700

    Add: buy-and-sell-crypto - submission-55

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-55.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int lowest = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++)
+        {
+            if(prices[i] < lowest)
+            {
+                lowest = prices[i];
+            }
+            int profit = prices[i] - lowest;
+            if (profit > maxProfit )
+            {
+                maxProfit = profit;
+            }
+        } 
+        return maxProfit;
+        
+    }
+}

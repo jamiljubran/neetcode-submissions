0f8d77637dc790dd4658a2e6dd35be7e793381c3
jamiljubran <jamiljubran@gmail.com>
2026-09-15 18:12:53 -0700

    Add: duplicate-integer - submission-40

---
+++ Data Structures & Algorithms/duplicate-integer/submission-40.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet <Integer> numSet = new HashSet<>();
+        for(int num : nums)
+        {
+            if(numSet.contains(num))
+            {
+                return true;
+            }
+            else
+            {
+                numSet.add(num);
+            }
+            
+        }
+        return false;
+        
+    }
+}
